efc26a82e12ee060c40834bc2f792d1e0fa7b5ad
Thomas <msamoht16@gmail.com>
2019-02-01 20:23:28 -0200

    Initial Commit

---
+++ wecast_playerTests/wecast_playerTests.swift
@@ -0,0 +1,34 @@
+//
+//  wecast_playerTests.swift
+//  wecast_playerTests
+//
+//  Created by Thomás Marques Brandão Reis on 01/02/19.
+//  Copyright © 2019 Thomás Marques Brandão Reis. All rights reserved.
+//
+
+import XCTest
+@testable import wecast_player
+
+class wecast_playerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
